fbd113735600a419e3ce6c1907f943d850b6d742
Kim Thu Tran <161789376+kimthu123@users.noreply.github.com>
2025-11-07 22:42:09 +1100

    Update account.py

--- domain/account.py
+++ domain/account.py
@@ -1,6 +1,10 @@
+"""Account base class module."""
+
+
 class Account:
+    """Base class representing a system account."""
+
     def __init__(self, email, password, account_type):
         self.email = email
         self.password = password
         self.account_type = account_type
-

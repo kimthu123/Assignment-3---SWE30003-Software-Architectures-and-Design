d91222ca71edf3768a29d649255b84384656812b
Kim Thu Tran <161789376+kimthu123@users.noreply.github.com>
2025-11-07 22:42:35 +1100

    Update admin.py

--- domain/admin.py
+++ domain/admin.py
@@ -1,6 +1,10 @@
+"""Admin account module."""
+
 from domain.account import Account
 
+
 class Admin(Account):
+    """Administrator account class."""
+
     def __init__(self, email, password):
         super().__init__(email, password, account_type="admin")
-
